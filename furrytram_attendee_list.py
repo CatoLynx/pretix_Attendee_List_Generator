2a6c02396d65cdae9c0e9d15437b6610054933e1
Julian Metzler <git@mezgr.de>
2023-12-15 10:42:47 +0100

    Handle pagination

--- furrytram_attendee_list.py
+++ furrytram_attendee_list.py
@@ -6,42 +6,51 @@ from config import *
 
 
 def api_call(path, method='get', payload=None):
+    if path.startswith(API_URL):
+        url = path
+    else:
+        url = API_URL + path
+    
     if method == 'get':
-        resp = requests.get(API_URL + path, headers={"Authorization": "Token " + API_TOKEN})
+        resp = requests.get(url, headers={"Authorization": "Token " + API_TOKEN})
     elif method == 'post':
-        resp = requests.post(API_URL + path, data=payload, headers={"Authorization": "Token " + API_TOKEN})
+        resp = requests.post(url, data=payload, headers={"Authorization": "Token " + API_TOKEN})
     return resp
 
 
 def main():
-    print("Getting orders")
-    resp = api_call(f"organizers/{ORGANIZER}/events/{EVENT}/orders")
-    if resp.status_code != 200:
-        print(f"  Error: HTTP status code {resp.status_code}")
-        return
-    
-    orders = resp.json()['results']
-    
     att_list = []
     att_list_no_consent = []
-    html = ""
+    
+    print("Getting orders")
+    data = {'next': f"organizers/{ORGANIZER}/events/{EVENT}/orders"}
+    
+    while data['next']:
+        print(f"  Querying {data['next']}")
+        resp = api_call(data['next'])
+        if resp.status_code != 200:
+            print(f"  Error: HTTP status code {resp.status_code}")
+            return
+        
+        data = resp.json()
+        orders = data['results']
 
-    for order in orders:
-        # Skip non-paid orders
-        if order['status'] != 'p':
-            continue
-        positions = order['positions']
-        for position in positions:
-            answered = False
-            for answer in position['answers']:
-                if answer['question'] == CONSENT_QUESTION_ID:
-                    answered = True
-                    if answer['answer'] == "True":
-                        att_list.append(position['attendee_name'])
-                    else:
-                        att_list_no_consent.append(position['attendee_name'])
-            if not answered:
-                att_list_no_consent.append(position['attendee_name'])
+        for order in orders:
+            # Skip non-paid orders
+            if order['status'] != 'p':
+                continue
+            positions = order['positions']
+            for position in positions:
+                answered = False
+                for answer in position['answers']:
+                    if answer['question'] == CONSENT_QUESTION_ID:
+                        answered = True
+                        if answer['answer'] == "True":
+                            att_list.append(position['attendee_name'])
+                        else:
+                            att_list_no_consent.append(position['attendee_name'])
+                if not answered:
+                    att_list_no_consent.append(position['attendee_name'])
     
     if not att_list:
         att_list = ["..."]
